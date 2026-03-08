32369e1742bf3c01e67e19580d04675de6b18bc8
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-08 18:37:36 -0300

    Add installation script for Hatter icon theme
    This script installs the Hatter icon theme by cloning the repository, copying the icons, updating the icon cache, and setting the theme in the GTK configuration.

--- files/scripts/install-hatter-icons.sh
+++ files/scripts/install-hatter-icons.sh
@@ -1,1 +1,14 @@
+#!/usr/bin/env bash
+set -e
 
+git clone --depth 1 https://github.com/Mibea/Hatter.git /tmp/Hatter
+
+mkdir -p /usr/share/icons
+cp -r /tmp/Hatter/Hatter /usr/share/icons/
+
+gtk-update-icon-cache /usr/share/icons/Hatter
+
+mkdir -p /etc/skel/.config/gtk-3.0
+echo "gtk-icon-theme-name=Hatter" > /etc/skel/.config/gtk-3.0/settings.ini
+
+rm -rf /tmp/Hatter
