b5c0828d57d36b2dd17de7ede9ac99d37d44625d
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-08 19:23:52 -0300

    Remove flatpak uninstall command for Fedora
    Removed command to uninstall all apps from the Fedora remote.

--- files/scripts/remove-fedora-flatpaks.sh
+++ files/scripts/remove-fedora-flatpaks.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-# remove todos os apps instalados do remote fedora
-flatpak uninstall --system --all --delete-data --noninteractive --from fedora || true
-
 # remove o remote fedora
 flatpak remote-delete fedora || true
