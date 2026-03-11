5fcaf175cce0e78e6ee89c11ff92f1fae317055f
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-11 09:56:42 -0300

    Add GNOME configuration script
    This script configures GNOME settings, including interface themes, window button layout, and favorite applications.

--- files/scripts/configure-gnome.sh
+++ files/scripts/configure-gnome.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
+set -e
 
+mkdir -p /etc/dconf/db/local.d
+
+cat > /etc/dconf/db/local.d/00-gnome-defaults << 'EOF'
+
+# Interface
+[org/gnome/desktop/interface]
+icon-theme='Hatter'
+clock-show-weekday=true
+enable-hot-corners=false
+
+# Windows
+[org/gnome/desktop/wm/preferences]
+button-layout='appmenu:minimize,maximize,close'
+
+# Dock / favorites apps
+[org/gnome/shell]
+favorite-apps=[
+'org.gnome.Nautilus.desktop',
+'org.gnome.Settings',
+'org.gnome.Software',
+'org.gnome.TextEditor.desktop',
+'org.gnome.Calculator.desktop',
+'org.onlyoffice.desktopeditors.desktop',
+'com.brave.Browser.desktop'
+]
+
+EOF
+
+dconf update
