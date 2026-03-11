7c3df12d2e9446be895cad55bef1d7e7e68fe8c0
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-11 09:44:22 -0300

    Refactor icon installation script for better error handling
    Replace mkdir with install -d for icon directory creation and ensure gtk-update-icon-cache does not fail.

--- files/scripts/install-hatter-icons.sh
+++ files/scripts/install-hatter-icons.sh
@@ -3,12 +3,9 @@ set -e
 
 git clone --depth 1 https://github.com/Mibea/Hatter.git /tmp/Hatter
 
-mkdir -p /usr/share/icons
+install -d /usr/share/icons
 cp -r /tmp/Hatter/Hatter /usr/share/icons/
 
-gtk-update-icon-cache /usr/share/icons/Hatter
-
-mkdir -p /etc/skel/.config/gtk-3.0
-echo "gtk-icon-theme-name=Hatter" > /etc/skel/.config/gtk-3.0/settings.ini
+gtk-update-icon-cache /usr/share/icons/Hatter || true
 
 rm -rf /tmp/Hatter
