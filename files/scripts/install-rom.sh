fd32d03ce88901a2cf2c04858d1498289ceb2251
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-11 18:03:52 -0300

    Simplify installation script by piping curl output

--- files/scripts/install-rom.sh
+++ files/scripts/install-rom.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-curl -fsSL https://raw.githubusercontent.com/diogopessoa/rpm-ostree-manager/main/install.sh -o /tmp/install-rom.sh
-bash /tmp/install-rom.sh
-rm -f /tmp/install-rom.sh
+curl -fsSL https://raw.githubusercontent.com/diogopessoa/rpm-ostree-manager/main/install.sh | bash
