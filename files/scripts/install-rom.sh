f3412e0b63cc6d8263762bd5f18e02c830a91166
Diogo <12633864+diogopessoa@users.noreply.github.com>
2026-03-11 09:10:22 -0300

    Modify install-rom.sh to use temporary script
    Update installation script to download and execute from temp.

--- files/scripts/install-rom.sh
+++ files/scripts/install-rom.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-curl -fsSL https://raw.githubusercontent.com/diogopessoa/rpm-ostree-manager/main/install.sh | bash
+set -e
+
+curl -fsSL https://raw.githubusercontent.com/diogopessoa/rpm-ostree-manager/main/install.sh -o /tmp/install-rom.sh
+bash /tmp/install-rom.sh
+rm -f /tmp/install-rom.sh
